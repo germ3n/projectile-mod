deb3d7ae037edd1a6c84bbb4c07174a82d888870
germ3n <gasfggit@proton.me>
2025-12-16 11:46:30 +0100

    print/Msg/etc are limited to 4096 chars (including null char, i think?) so we print 4095. 4096 skips a char

--- lua/projectiles/backup.lua
+++ lua/projectiles/backup.lua
@@ -37,15 +37,60 @@ function projectiles_backup_config(type, flags)
     return nil;
 end
 
+function projectiles_restore_config(data)
+    if data["surfaceprops"] then
+        table.Merge(SURFACE_PROPS_PENETRATION, data["surfaceprops"]);
+        print("restored surfaceprops");
+    end
+
+    if data["weapon_config"] then
+        table.Merge(CONFIG_TYPES, data["weapon_config"]);
+        print("restored weapon config");
+    end
+    
+    if data["cvars"] then
+        for cvar, value in next, data["cvars"] do
+            RunConsoleCommand(cvar, value);
+        end
+
+        print("restored cvars");
+    end
+
+    print("restored projectiles config");
+
+    --PrintTable(data);
+    --todo: add syncing to clients
+end
+
 local PROJECTILES_BACKUP_ALL = PROJECTILES_BACKUP_ALL;
 local NULL = NULL; 
 
 local player_meta = FindMetaTable("Player");
 local is_superadmin = player_meta.IsSuperAdmin;
 
-concommand.Add("projectiles_backup_config_json", function(ply, cmd, args)
+concommand.Add("pro_config_backup_json", function(ply, cmd, args)
     if ply ~= NULL and (not is_superadmin(ply)) then return; end
     local flags = PROJECTILES_BACKUP_ALL;
     local backup = projectiles_backup_config("json", flags);
-    print(backup);
+    local chunk_size = 4095;
+    local chunks = math.ceil(string.len(backup) / chunk_size);
+    for i = 1, chunks do
+        local start_pos = (i - 1) * chunk_size + 1;
+        local end_pos = math.min(start_pos + chunk_size, string.len(backup));
+        local chunk = string.sub(backup, start_pos, end_pos);
+        Msg(chunk);
+    end
+    
+    --TEST_DATA = backup;
+
+    print("\nbackup complete");
 end, nil, "Backup projectiles config");
+
+concommand.Add("pro_config_restore_json", function(ply, cmd, args)
+    if ply ~= NULL and (not is_superadmin(ply)) then return; end
+    --local data = util.JSONToTable(args[1]);
+    --local data = util.JSONToTable(TEST_DATA);
+    --projectiles_restore_config(data);
+
+    print("not yet implemented");
+end, nil, "Restore projectiles config");
